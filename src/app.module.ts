c41e07260a6ddb105a97bb4a51632d8499a539e4
houssinCarnelian <houssin.carnelian@gmail.com>
2023-07-14 16:15:12 +0100

    add the typeorm postresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule, UsersModule],
+  imports: [CoffeesModule, UsersModule,TypeOrmModule.forRoot({
+    type: 'postgres',
+    database: 'testDB',
+    host: 'localhost',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    autoLoadEntities: true,
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
